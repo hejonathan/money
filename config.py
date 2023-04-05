78f434b84d94c02ffdd85e3ffdb693d319da8ac7
chote <chotepong.tangton@gmail.com>
2023-04-05 00:47:16 -0500

    config and requirements

---
+++ config.py
@@ -0,0 +1,11 @@
+API_KEY = "PK2XNKDSJH4PVPCYDK8E"
+SECRET_KEY = "f5cD1kbR3p5RRfgvNgDiQ09qRpi4LCYmPZprl3KM"
+
+ALPACA_CONFIG = {
+# Put your own Alpaca key here:
+"API_KEY": "PK2XNKDSJH4PVPCYDK8E",
+# Put your own Alpaca secret here:
+"API_SECRET": "f5cD1kbR3p5RRfgvNgDiQ09qRpi4LCYmPZprl3KM",
+# If you want to go live, you must change this
+"ENDPOINT": "https://paper-api.alpaca.markets",
+}
