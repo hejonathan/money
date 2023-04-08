8141136fa41426a28a08e17f5edb9ea07834addf
Ruchir <ruchir.adenwala@gmail.com>
2023-04-08 16:24:48 -0500

    Testing class created, not made into a header yet

--- testing_class.py
+++ testing_class.py
@@ -27,7 +27,7 @@ class Test:
         print("Start date: ", self.start_date)
         print("End date: ", self.end_date)
 
-
+# testing
 test1 = Test()
 test1.get_comp_list()
 test1.get_start_date()
